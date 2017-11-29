b265147a3064f662c9e53e35bd65d4e149b9cc80
andiankas <rustiantoandi17@gmail.com>
2017-11-29 16:36:59 +0700

    create file myapp.js

---
+++ myapp.js
@@ -0,0 +1,24 @@
+// Aplikasi untuk mengetahui generasi apakah kamu sebenearnya?
+// ---------------------
+// input tahun lahir dan simpan
+// cetak tahun lahir yang telah diinput tadi
+
+// (Tahun lahir dibawah tahun 1964 merupakan GENERASI BABY BOOMER)
+// Jika tahun lahir kurang dari tahun 1964
+  // Cetak Generasi Baby Boomer
+
+// (Tahun lahir 1965 - 1982 merupakan GENERASI X)
+  // jika tahun lahir kurang dari tahun 1982
+  // cetak generasi X
+  
+// (Tahun lahir 1983 - 1997 merupakan GENERASI Y)
+  // jika tahun lahir kurang dari 1997
+  // cetak generasi Y
+
+// (Tahun lahir 1998 - 2009 merupakan GENERASI Z)
+  // jika tahun lahir kurang dari 2009
+  // cetak GENERASI Z
+  
+// (Tahun lahir lebih dari 2010 merupakan GENERASI ALPHA)
+  // jika tahun lahir lebih dari 2010
+  // cetak Generasi ALPHA
